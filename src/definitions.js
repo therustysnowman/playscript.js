4bd2cfa235fd0bac32912bb6562c2eecd5ded4a7
Justin Klim <justin@klim.co.uk>
2017-05-04 23:14:53 +0100

    Rename 'assetLibrary' to 'definitions' and add test cases

---
+++ src/definitions.js
@@ -0,0 +1,61 @@
+
+module.exports = Definitions;
+
+var extend = require("./util.js").extend;
+
+function Definitions() {
+
+  var _defs  = {};
+
+  function _get(type, name) {
+    if (_defs[type] && _defs[type][name]) {
+      return _defs[type][name];
+    } else {
+      throw new Error(
+        "No asset of type '" + type + "' with name '" + name + "' found."
+      );
+    }
+  }
+
+  function _build(type, def) {
+    var ret = {};
+    if (typeof def === "string") {
+      def = _get(type, def);
+    }
+    if (typeof def.extends === "string") {
+      extend(ret, _build(type, def.extends));
+    }
+    extend(ret, def);
+    if (typeof ret.extends !== "undefined") {
+      delete ret.extends;
+    }
+    return ret;
+  }
+
+  this.add = function(type, name, def) {
+
+    if (typeof _defs[type] === "undefined") {
+      _defs[type] = {};
+    }
+
+    if (typeof _defs[type][name] !== "undefined") {
+      throw new Error(
+        "Cannot add '" + type +
+        "' asset as name '" + name +
+        "' is already used."
+      );
+    }
+
+    _defs[type][name] = def;
+  };
+
+  /**
+  * opt can be:
+  * - the id of an asset
+  * - a definition that may (or may not) include opt.info.extends as another
+  *   asset to build on top of
+  */
+  this.get = function(type, opt) {
+    return _build(type, opt);
+  };
+}
---
+++ test/test_definitions.js
@@ -0,0 +1,150 @@
+var expect = require('chai').expect;
+var Definitions = require('../src/definitions');
+
+var defs = new Definitions();
+
+var A_BASE = {
+  first: {
+    a1: "first-a1-base",
+    b1: "first-b1-base"
+  }
+};
+
+var A_EXTENSION = {
+  extends: "A_BASE",
+  first: {
+    a1: "first-a1-ext",
+    c1: "first-c1-ext"
+  },
+  second: {
+    a2: "second-a2-ext"
+  }
+};
+
+var A_UNKNOWN = {
+  extends: "_UNKNOWN_"
+};
+
+defs.add("TYPE-A", "A_BASE", A_BASE);
+defs.add("TYPE-A", "A_EXTENSION", A_EXTENSION);
+defs.add("TYPE-A", "A_UNKNOWN", A_UNKNOWN);
+
+describe('assetLibrary', function() {
+
+  beforeEach(function() {
+  });
+
+  it('add', function() {
+    defs.add("TYPE-A", "ADD_TEST", { item: "value" } );
+    var def = defs.get("TYPE-A", "ADD_TEST");
+    expect(def).to.have.a.property("item").that.equals("value");
+  });
+
+  it('add duplicate', function() {
+    expect(function() {
+      defs.add("TYPE-A", "A_BASE", A_BASE);
+    }).to.throw("Cannot add 'TYPE-A' asset as name 'A_BASE' is already used.");
+  });
+
+  it('get by name unknown', function() {
+    expect(function() {
+      defs.get("TYPE-A", "_UNKNOWN_");
+    }).to.throw("No asset of type 'TYPE-A' with name '_UNKNOWN_' found.");
+  });
+
+  it('get by name without extends', function() {
+
+    var def = defs.get("TYPE-A", "A_BASE");
+
+    expect(def).to.deep.equal({
+      first: {
+        a1: "first-a1-base",
+        b1: "first-b1-base"
+      }
+    });
+  });
+
+  it('get by name with extends', function() {
+
+    var def = defs.get("TYPE-A", "A_EXTENSION");
+
+    expect(def).to.deep.equal({
+      first: {
+        a1: "first-a1-ext",
+        b1: "first-b1-base",
+        c1: "first-c1-ext"
+      },
+      second: {
+        a2: "second-a2-ext"
+      }
+    });
+  });
+
+  it('get by name with unknown extends in chain', function() {
+    expect(function() {
+      defs.get("TYPE-A", "A_UNKNOWN");
+    }).to.throw("No asset of type 'TYPE-A' with name '_UNKNOWN_' found.");
+  });
+
+  it('get by definition without extends', function() {
+
+      var def = defs.get("TYPE-A", {
+        first: {
+          z1: "first-z1-def"
+        },
+        second: {
+          z2: "second-z2-def"
+        }
+      });
+
+      expect(def).to.deep.equal({
+        first: {
+          z1: "first-z1-def"
+        },
+        second: {
+          z2: "second-z2-def"
+        }
+      });
+  });
+
+  it('get by definition with extends', function() {
+
+    var def = defs.get("TYPE-A", {
+      extends: "A_EXTENSION",
+      first: {
+        a1: "first-a1-def",
+        d1: "first-d1-def"
+      },
+      second: {
+        a2: "second-a2-def",
+        b2: "second-b2-def"
+      },
+      third: {
+        a3: "third-a3-def"
+      }
+    });
+
+    expect(def).to.deep.equal({
+      first: {
+        a1: "first-a1-def",
+        b1: "first-b1-base",
+        c1: "first-c1-ext",
+        d1: "first-d1-def"
+      },
+      second: {
+        a2: "second-a2-def",
+        b2: "second-b2-def"
+      },
+      third: {
+        a3: "third-a3-def"
+      }
+    });
+  });
+
+  it('get by definition with unknown extends', function() {
+    expect(function() {
+      defs.get("TYPE-A", { extends: "_UNKNOWN_" } );
+    }).to.throw("No asset of type 'TYPE-A' with name '_UNKNOWN_' found.");
+  });
+
+});
